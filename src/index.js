c342d108797f09218b74f7084e0520ab19f6e733
umutaaslan <umutalperaslan705@hotmail.com>
2025-02-24 20:12:20 +0300

    there is a bug

--- src/index.js
+++ src/index.js
@@ -1,43 +1,70 @@
-/* eslint-disable */ 
+/* eslint-disable */
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import Globe from "globe.gl";
 
- 
+fetch("/geo.json", { mode: "cors" })
+  .then((res) => res.json())
+  .then((geoJson) => {
+    fetch("/geoq.json", { mode: "cors" })
+      .then((res) => res.json())
+      .then((geoPJson) => {
+        const geoJsonData = geoJson.features.filter((d) => d.geometry != null);
+        const countriesArr = geoJsonData.filter(
+          (d) => d.properties.ISO_A2 !== "AQ",
+        );
 
-fetch("/geo.json", {mode: "cors"}).then(res => res.json()).then(geoJson => {
-    console.log(geoJson)
-    const geoJsonData = geoJson.features.filter(d => d.geometry != null);
-    console.log(geoJsonData.features)
-
-    let lastHoverD = null;
-
-
-    const globe = new Globe(document.querySelector("#showGlobe"))
-        .globeImageUrl("dayEarth.jpg")
-        .showAtmosphere(true)
-        .atmosphereColor("lightskyblue")
-        .polygonsData(geoJsonData.filter(d => d.properties.ISO_A2 !== 'AQ'))
-        .polygonAltitude(0.04)
-        .polygonStrokeColor("red")
-        .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
-        .polygonStrokeColor(() => '#111')
-        .onPolygonHover((hoverD, lastHoverD) => { 
-            if(hoverD !== lastHoverD){
-                globe
-                .polygonAltitude(d => d === hoverD ? 0.08 : 0.04)
-                .polygonCapColor(d => d === hoverD ? 'steelblue' : "#ffaa00")    
+        const globe = new Globe(document.querySelector("#showGlobe"))
+          .globeImageUrl("dayEarth.jpg")
+          .showAtmosphere(true)
+          .atmosphereColor("lightskyblue")
+          .polygonsData(countriesArr)
+          .polygonAltitude(0.04)
+          .polygonStrokeColor("red")
+          .polygonSideColor(() => "rgba(0, 100, 0, 0.15)")
+          .polygonStrokeColor(() => "#111")
+          .onPolygonHover((hoverD, lastHoverD) => {
+            if (hoverD !== lastHoverD) {
+              globe
+                .polygonAltitude((d) => (d === hoverD ? 0.08 : 0.04))
+                .polygonCapColor((d) =>
+                  d === hoverD ? "steelblue" : "#ffaa00",
+                );
             }
+          })
+          .polygonsTransitionDuration(250)
+          .onPolygonClick((polygon) => {
+            console.log(polygon);
+          });
 
-        })
-        .polygonsTransitionDuration(250)
-    
-    
-    globe.onGlobeClick(({lat, lng}, e) => {
-        console.log(lat, lng)
-    })
+        function drawProvincesBorders() {
+          const newArr = [];
+          for (let feature of geoPJson.features) {
+            if (feature.properties.iso_a2 == "TR") {
+              newArr.push(feature);
+            }
+          }
 
-   
-    
-});
+          // const result = countriesArr.concat(newArr);
+
+          globe.polygonsData([]);
+          globe.polygonsData(newArr);
+          //try geometryPolygonsData
+          //as last remedy, code a function by yourself that draws province borders
+        }
+        drawProvincesBorders();
+
+        // globe.onGlobeClick(({lat, lng}, e) => {
+        //     console.log(lat, lng)
+        // })
+
+        // function triggerCountry(countryISO){
+        //     //handle camera focus (make it static unless focus is being losed)
+
+        //     //show provinces
+        //     drawProvincesBorders(countryISO);
+
+        // }
+      });
+  });
